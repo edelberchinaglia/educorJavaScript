9e75f61310eb59711cb0239a62f779f7c951a5cc
edelberchinaglia <edelber.chinaglia@gmail.com>
2022-10-07 14:51:59 -0300

    Trabalhando com variaveis

---
+++ 002/app.js
@@ -0,0 +1,18 @@
+console.log("Encontro 2")
+// Sintaxe javascript, variáveis e tipos de dados - ctrl; faz comentário de uma linha /* e */ faz comentários de trechos
+//variáveis
+nome="Edelber"
+console.log(nome)
+var nome="Gigica"
+console.log(nome)
+var nome="Chico"           //declara variavel com escopo global
+console.log(nome)
+let sobrenome="chinaglia"  //declara variavel, mas não deixa declarar novamente mais pra baixo e tem escopo local
+console.log(sobrenome) 
+if (10>3){
+    console.log("10 é maior que 3")
+    let idade=21
+    console.log(idade)
+}
+let mídia="tv"
+console.log(mídia)
